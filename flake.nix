304555e630fd189c8bf1c4d5e72221d4e69308a0
tybuu <tybuu1234@gmail.com>
2025-07-27 06:05:55 -0700

    switched to stable as unstable broke!!!

--- flake.nix
+++ flake.nix
@@ -4,12 +4,12 @@
   inputs = {
     # Nixpkgs
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-osu.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # Home manager
     # home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.url = "github:nix-community/home-manager/";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
