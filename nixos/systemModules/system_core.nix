7388fc3a18206f5344cb376850e42be69e407701
tybuu <tybuu1234@gmail.com>
2026-09-02 07:14:43 -0700

    Got nixos on mac working

--- nixos/systemModules/system_core.nix
+++ nixos/systemModules/system_core.nix
@@ -189,7 +189,7 @@ in {
   hardware.graphics = {
     enable = true;
     # package = pkgs-unstable.mesa;
-    enable32Bit = true;
+    #enable32Bit = true;
     # package32 = pkgs-unstable.pkgsi686Linux.mesa;
   };
 
